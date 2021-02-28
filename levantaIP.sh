89ed28cee48a5f91982589361f377c7fb9ab25ff
Adrian González Pardo <gozapaadr@gmail.com>
2021-02-28 17:02:32 -0600

    New script for turn on the interface and check if the execute of the script is root

--- levantaIP.sh
+++ levantaIP.sh
@@ -3,7 +3,6 @@ if [ "$EUID" -ne 0 ];then
   echo "Error, please execute the script as root"
   exit 1
 fi
-
 echo -n "Ingresa el nombre de la interfaz de red: "
 read interface
 echo -n "Ingresa la ip que deseas utilizar para la interfaz: "
@@ -12,6 +11,7 @@ echo -n "Ingresa el prefijo que utilizas para la interfaz ${interface}: "
 read prefix
 echo -n "Ingresa la ip del gateway: "
 read gate
+ip l s dev ${interface} up
 ip route flush dev ${interface}
 ip addr add ${ip}/${prefix} dev ${interface}
 ip route add default via ${gate}
